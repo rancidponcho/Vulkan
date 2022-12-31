0a5ba30d1f66bf8b1d1657245a78f9b5434ce4dc
rancidponcho <milo.kasparek@gmail.com>
2022-12-30 18:20:10 -0600

    Initialized & created VkInstance

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ public:
 	}
 
 private:
-
-	GLFWwindow* window;
-
+	GLFWwindow* window;		// actual window of app
+	VkInstance instance;	// connection between app & the Vk library
+	
 	void initWindow() {
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);	// GLFW originally meant for OpenGL
@@ -31,9 +31,9 @@ private:
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	
 	}	
-	
-	void initVulkan() {
 
+	void initVulkan() {
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -43,9 +43,53 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);	// optional allocator callback ignored by passing nullptr
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
+	}
+
+	void createInstance() {
+		/*
+			The general pattern that object creation function parameters in follow is:
+				1) Pointer to struct with creation info
+				2) Pointer to custom allocator callbacks (ignored by passing nullptr to vkDestroyInstance())
+				3) Pointer to the variable that stores the handle to the new object
+		*/
+		//	1) OPTIONAL
+		//	Provides Vk driver with application information for optimization on different devices.
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; 
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+		
+		//	2) OPTIONAL
+		//	Using nullptr for allocator callback
+
+		//	3) REQUIRED
+		//	Tells Vk driver which global extensions and validation layers to use. 
+		//	"Global" means it applies to the ENTIRE program & not a specific device.
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		//	Vk is platform agnostic -> we need an extension to interface with the window system.
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		//	We must find required extension(s) of windowing application (GLFW)	
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		//	Global validation layers must be determined and enabled
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		//	Now that parameters are specified, a Vk instance can be created & checked for success
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}	
 	}
 };
 
