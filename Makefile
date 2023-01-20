2f65c5fef478b43baf6e94dabdc463187c21d1ce
rancidponcho <milo.kasparek@gmail.com>
2023-01-19 21:12:22 -0600

    validation layer tools

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
-CFLAGS = -std=c++17 -O2
+CFLAGS = -std=c++17 -O2 -g
 
 # GLFW, VK function loader, low level systems for GLFW 
 LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 VulkanTest: main.cpp
 	g++ $(CFLAGS) -o VulkanTest main.cpp $(LDFLAGS)
-
-
--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
-#include <vulkan/vulkan.h>		// LunarG SDK
-
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <string>
+
+#include <vulkan/vulkan.h> // LunarG SDK
 
 #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>			// Cross-platform window for graphics libraries (GL)
+#include <GLFW/glfw3.h> // Cross-platform window for graphics libraries (GL)
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication {
+// validation layers
+const std::vector<const char *> validationLayers = {
+	"VK_LAYER_KHRONOS_validation" // standard validation is bundled in this specification
+};
+
+#ifndef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication
+{
 public:
-	void run() {
+	/* */
+	void run()
+	{
 		initWindow();
 		initVulkan();
 		mainLoop();
@@ -20,85 +36,176 @@ public:
 	}
 
 private:
-	GLFWwindow* window;		// actual window of app
-	VkInstance instance;	// connection between app & the Vk library
-	
-	void initWindow() {
-		glfwInit();
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);	// GLFW originally meant for OpenGL
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);		// window resizing requires extra care
-		
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-	
-	}	
+	GLFWwindow *window;	 // actual window of app
+	VkInstance instance; // connection between app & the Vk library
 
-	void initVulkan() {
+	// INITIALIZATION //
+	void initWindow()
+	{
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // GLFW originally meant for OpenGL
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);	  // window resizing requires extra care
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan()
+	{
 		createInstance();
 	}
 
-	void mainLoop() {
-		while (!glfwWindowShouldClose(window)) {
-			glfwPollEvents();
+	void createInstance()
+	{
+		// supportedExtensions();
+
+		if (enableValidationLayers && !checkValidationLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but not available!");
 		}
-	}
 
-	void cleanup() {
-		vkDestroyInstance(instance, nullptr);	// optional allocator callback ignored by passing nullptr
-
-		glfwDestroyWindow(window);
-
-		glfwTerminate();
-	}
-
-	void createInstance() {
 		/*
 			The general pattern that object creation function parameters in follow is:
 				1) Pointer to struct with creation info
-				2) Pointer to custom allocator callbacks (ignored by passing nullptr to vkDestroyInstance())
+				2) Pointer to custom allocator callbacks
 				3) Pointer to the variable that stores the handle to the new object
 		*/
-		//	1) OPTIONAL
-		//	Provides Vk driver with application information for optimization on different devices.
+
+		//	1)	Provides Vk driver with application information for optimization on different devices.
 		VkApplicationInfo appInfo{};
-		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; 
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Hello Triangle";
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
-		
-		//	2) OPTIONAL
-		//	Using nullptr for allocator callback
 
-		//	3) REQUIRED
-		//	Tells Vk driver which global extensions and validation layers to use. 
-		//	"Global" means it applies to the ENTIRE program & not a specific device.
+		//	2)	Using nullptr for allocator callback
+
+		//	3)	Tells Vk driver which global extensions and validation layers to use.
+		//	    "Global" means it applies to the ENTIRE program & not a specific device.
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
+
 		//	Vk is platform agnostic -> we need an extension to interface with the window system.
 		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions;
-		//	We must find required extension(s) of windowing application (GLFW)	
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		const char **glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount); //	find required extension(s) of windowing application (GLFW)
+
 		//	Global validation layers must be determined and enabled
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
-		createInfo.enabledLayerCount = 0;
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
 
 		//	Now that parameters are specified, a Vk instance can be created & checked for success
-		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
 			throw std::runtime_error("failed to create instance!");
-		}	
+		}
+	}
+
+	/* ========== UTILITIES ========== */
+	// lists supported extensions and those that are glfw-required
+	void supportedExtensions()
+	{
+		// available VK extensions
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		std::cout << "availiable extensions:\n";
+
+		// glfw-required extensions
+		uint32_t glfwExtensionCount = 0;
+		const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		if (glfwExtensionCount == 0)
+		{
+			throw std::runtime_error("Failed to find GLFW extensions!");
+		}
+
+		// lists to terminal
+		for (const auto &extension : extensions)
+		{
+			std::cout << '\t' << extension.extensionName;
+
+			// check if required for glfw
+			for (int i = 0; i < glfwExtensionCount; i++)
+			{
+				std::string extensionName = glfwExtensions[i];
+				if (extension.extensionName == extensionName)
+				{
+					std::cout << " -- GLFW-REQUIRED";
+				}
+			}
+			std::cout << '\n';
+		}
+	}
+
+	bool checkValidationLayerSupport()
+	{
+		// available validation layers
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char *layerName : validationLayers)
+		{
+			bool layerFound = false;
+
+			for (const auto &layerProperties : availableLayers)
+			{
+				if (layerName == layerProperties.layerName)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/* ========== MAIN LOOP ========== */
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		vkDestroyInstance(instance, nullptr); // optional allocator callback ignored by passing nullptr
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
 	}
 };
 
-int main() {
+int main()
+{
 	HelloTriangleApplication app;
 
-	try {
+	try
+	{
 		app.run();
-	} catch (const std::exception &e) {
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
